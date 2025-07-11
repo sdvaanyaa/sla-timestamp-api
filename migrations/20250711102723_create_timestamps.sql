8915a826ad1290b44153b60d47517c8d77d43540
Ivan A <sd.vaanyaa@yandex.ru>
2025-07-11 21:58:05 +0300

    add .gitignore, migrations and go.sum

---
+++ migrations/20250711102723_create_timestamps.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE timestamps (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    external_id VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    tag VARCHAR(50) NOT NULL,
+    stage VARCHAR(50) NOT NULL,
+    meta JSONB
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS timestamps;
+DROP EXTENSION IF EXISTS "uuid-ossp";
+-- +goose StatementEnd
